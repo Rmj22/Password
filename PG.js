9b33f7f5c0284dedd5466e1a6c61674e473d78e2
Robin Jones <RobinJones335@gmail.com>
2020-04-03 01:34:11 -0400

    fixed styling

--- PG.js
+++ PG.js
@@ -1,15 +1,12 @@
 var generate = document.querySelector("#generate");
 var copyPassword = document.querySelector("#password_done");
-
-//Arrays
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = lowercase.toUpperCase();
 var numbers = "0123456789";
-// use "\" to define the (") as a string 
 var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 
-// check for character length
+
 function getCharacterLength(){
     while (characterLength === "null"){
         console.log(characterLength);
@@ -24,14 +21,13 @@ function getCharacterLength(){
     } 
 }
 
-// check to see if user wants special characters
 function getSpecialChar(){
     while (useSpecialChar === "null"){
         useSpecialChar = confirm("Did you add special characters?");
         console.log(useSpecialChar);
     }
 }
-// function that selects a random string from passwordBowl and then adds it to passwordFinal.
+
 function createPassword(){
     if (useSpecialChar){
         passwordBowl = lowercase + uppercase + numbers + specialChar;
